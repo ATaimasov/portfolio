0d90f5a69e4c6ebb8429a1da8ea934adf24f6218
Alexander Taimasov <alexander.taimasov@gmail.com>
2024-06-30 13:06:46 +0300

    added font-icons, made fixes for the adaptive

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+;(function ($, undefined) {
+        $('.slider').slick( {
+           arrows: true,
+           dots: true,
+           adaptiveHeight: true,
+           slidesToShow: 2,
+           slidesToScroll: 2,
+           speed: 800,
+           easing: 'ease',
+           infinite: true,
+           initialSlide: 0,
+           autoplay: false,
+           autoplaySpeed: 3000,
+           pauseOnFocus: true,
+           pauseOnDotsHover: true,
+           pauseOnHover: true,
+           draggable: true,
+           swipe: true,
+           touchTreshold: 10,
+           touchMove: true,
+           waitForAnimate: true,
+           centerMode: false,
+           variableWidth: false,
+        })
+          
+        
+    
+    }) (jQuery);
