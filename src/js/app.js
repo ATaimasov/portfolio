0ac14014d5c65ae7968076e533dd9c3975cf479a
Alexander Taimasov <136252899+ATaimasov@users.noreply.github.com>
2024-07-25 23:44:30 +0300

    work with work-experience page

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,54 @@
 
+// libs
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+ const swiperBeforeIt = new Swiper('.swiper-before-it', {
+    modules: [Navigation, Pagination],
+    pagination: {
+        el: ".swiper-pagination",
+      },
+    grabCursor: true,
+    effect: "flip",
+    speed: 500,
+    loop: false,
+    clickable: true,
+    mousewheel: {
+    invert: false,
+    sensitivity: 1,
+  },
+  });
+
+  swiperBeforeIt.enable();
+
+
+  const swiperAfterIt = new Swiper('.swiper-after-it', {
+    modules: [Navigation, Pagination],
+    pagination: {
+        el: ".swiper-pagination-2",
+      },
+    grabCursor: true,
+    effect: "flip",
+    speed: 500,
+    loop: false,
+    clickable: true,
+    mousewheel: {
+    invert: false,
+    sensitivity: 1,
+  },
+  });
+
+  swiperAfterIt.enable();
+
+// basemodules
+import { isWebp } from './base-modules/webpCheck.js';
+isWebp();
+
+// blocks
 import { burger } from '../blocks/header-nav/header-nav.js';
 burger();
 
 import { scrollFading } from '../blocks/header-nav/header-nav.js';
 scrollFading();
 
-
-import { isWebp } from './base-modules/webpCheck.js';
-isWebp();
 
