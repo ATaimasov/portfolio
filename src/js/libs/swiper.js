0761bb4bebd8001878f4d4fee376062fed94da76
Alexander Taimasov <136252899+ATaimasov@users.noreply.github.com>
2024-07-28 23:51:57 +0300

    work with projects page

--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Pagination, EffectCube, Mousewheel} from 'swiper/modules';
+import { Pagination, EffectCube, Mousewheel, EffectFade} from 'swiper/modules';
 
  export const swiperBeforeIt = new Swiper('.swiper-before-it', {
     modules: [Pagination, EffectCube, Mousewheel ],
@@ -35,3 +35,24 @@ import { Pagination, EffectCube, Mousewheel} from 'swiper/modules';
  
 
   });
+
+  export const swiperProjects = new Swiper('.swiper-projects', {
+    modules: [Pagination, EffectFade, Mousewheel],
+    grabCursor: true,
+    speed: 1000,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+    invert: false,
+    sensitivity: 1,
+  },
+  
+  pagination: {
+    el: ".swiper-pagination__projects",
+    clickable: true,
+  },
+ 
+
+  });
+
+  
